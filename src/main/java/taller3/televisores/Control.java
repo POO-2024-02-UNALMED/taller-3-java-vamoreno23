720e680ed2f27f9801435442db128a6958bbd3b3
vamoreno23 <152014036+vamoreno23@users.noreply.github.com>
2024-11-19 11:03:59 -0500

    prueba

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,43 @@
+package taller3.televisores;
+
+public class Control {
+	private TV tv;
+	
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		tv.setControl(this);
+	}
+	
+	public void turnOn() {
+		this.tv.turnOn();
+	}
+	public void turnOff() {
+		this.tv.turnOff();
+	}
+	public void canalUp() {
+		this.tv.canalUp();
+	}
+	public void canalDown() {
+		this.tv.canalDown();
+	}
+	public void volumenUp() {
+		this.tv.volumenUp();
+	}
+	public void volumenDown() {
+		this.tv.volumenDown();
+	}
+
+	public void setCanal(int canal) {
+		this.tv.setCanal(canal);
+	}
+	public void setVolumen(int volumen) {
+		this.tv.setVolumen(volumen);
+	}
+	
+	public TV getTv() {
+		return this.tv;
+	}
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+}
---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca(String nombre) {
+		this.setNombre(nombre);
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
